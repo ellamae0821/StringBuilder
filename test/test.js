e5e7af5d0884929c007e62a57c4e544c233d7a6c
Ella Mae A <ellamae207@gmail.com>
2017-09-27 14:05:44 -1000

    1st test created

--- test/test.js
+++ test/test.js
@@ -3,4 +3,39 @@ const expect = chai.expect;
 const should = chai.should();
 const StringBuilder = require('.../string-builder');
 
-describe()
+describe('reverse', function (){
+
+  it('should reverse string', function (){
+    expect(reverse("hello").to.equal("olleh"));
+  });
+
+});
+
+describe.skip('capitaize', function (){
+  it('should capitaized first letter of a word', function (){
+  expect(reverse("hello world").to.equal("Hello World"));
+  });
+});
+
+
+describe.skip('toArray', function (){
+  var array;
+  it('should put the string in an array', function (){
+  toArray(array, hello);
+  expect(array).to.deep.equal("[hello]");
+  });
+});
+
+describe.skip('append', function (){
+  it('should append ', function (){
+  expect(reverse("hello world").to.equal("Hello World"));
+  });
+});
+
+describe.skip('toString', function (){
+  it('should capitaized first letter of a word', function (){
+  expect(reverse("hello world").to.equal("Hello World"));
+  });
+});
+
+
